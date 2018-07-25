d838fdf1069746b3937a275b1d861194ccf4758e
Ahmadali <ahmadali.azizov@outlook.com>
2018-07-25 17:53:42 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,8 +12,9 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  arr = ["1", "2", "3", "4"]
-  arr.shift["wow"]
+  array = ["I", "am", "really", "learning"]
+    element = "wow"
+    array.shift[element]
 end
 
 def remove_element_from_end_of_array(array)
